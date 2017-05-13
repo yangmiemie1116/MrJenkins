d474ed5268ad84e8a87805a1e4f352ffc8e93d8f
杨志强 <yangzhiqiang@hujiang.com>
2017-05-13 23:47:32 +0800

    archive ipa

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 set -e
-xcodebuild -workspace Mrjenkins.xcworkspace -scheme Mrjenkins -configuration Debug -sdk iphonesimulator clean build
+xcodebuild -workspace Mrjenkins.xcworkspace -scheme Mrjenkins -archivePath build/Mrjenkins.xcarchive archive
+
+xcodebuild -exportArchive -exportFormat IPA -archivePath build/Mrjenkins.xcarchive/ -exportPath build/Mrjenkins.ipa
