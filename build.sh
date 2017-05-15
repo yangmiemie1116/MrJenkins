3731c90b6ae73e14bc782df8cdd9532265f97cee
杨志强 <yangzhiqiang@hujiang.com>
2017-05-15 22:55:48 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 set -e
 rm -rf build/*
-pod install
 xcodebuild -workspace Mrjenkins.xcworkspace -scheme Mrjenkins -archivePath build/Mrjenkins.xcarchive archive
 
 xcodebuild -exportArchive -exportFormat IPA -archivePath build/Mrjenkins.xcarchive/ -exportPath build/Mrjenkins.ipa
